c833fd77b2b33761d5e9656a2b1a89510af69d08
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2016-09-01 18:00:56 +0200

    more classes

---
+++ src/antlr/v4/runtime/atn/TransitionStates.d
@@ -0,0 +1,20 @@
+module antlr.v4.runtime.atn.TransitionStates;
+
+// Enum TransitionStates
+/**
+ * TODO add enumeration class description
+ */
+enum TransitionStates
+{
+    INVALID,
+    EPSILON,
+    RANGE,
+    RULE,
+    PREDICATE,
+    ATOM,
+    ACTION,
+    SET,
+    NOT_SET,
+    WILDCARD,
+    PRECEDENCE,
+}

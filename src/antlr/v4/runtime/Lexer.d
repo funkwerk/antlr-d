bbff031593c7ca881c9d628b3cdacbe983f71919
Egbert Voigt <Egbert.Voigt@gmx.de>
2016-12-04 16:40:00 +0100

    more Lexer funtions

--- src/antlr/v4/runtime/Lexer.d
+++ src/antlr/v4/runtime/Lexer.d
@@ -1,14 +1,31 @@
 module antlr.v4.runtime.Lexer;
 
+import antlr.v4.runtime.Recognizer;
+import antlr.v4.runtime.atn.LexerATNSimulator;
+import antlr.v4.runtime.Token;
+import antlr.v4.runtime.CharStream;
+
 // Class Lexer
 /**
  * TODO add class description
  */
-abstract class Lexer
+abstract class Lexer : Recognizer!(int, LexerATNSimulator)
 {
 
     public static immutable int MIN_CHAR_VALUE = char.min;
 
     public static immutable int MAX_CHAR_VALUE = char.max;
+
+    public static immutable int DEFAULT_MODE = 0;
+
+    public static immutable int MORE = -2;
+
+    public static immutable int SKIP = -3;
+
+    public static immutable int DEFAULT_TOKEN_CHANNEL = Token.DEFAULT_CHANNEL;
+
+    public static immutable int HIDDEN = Token.HIDDEN_CHANNEL;
+
+    public CharStream _input;
 
 }
---
+++ src/antlr/v4/runtime/atn/LexerATNSimulator.d
@@ -0,0 +1,14 @@
+module antlr.v4.runtime.atn.LexerATNSimulator;
+
+import antlr.v4.runtime.atn.ATNSimulator;
+
+// Class LexerATNSimulator
+/**
+ * TODO add class description
+ */
+class LexerATNSimulator : ATNSimulator
+{
+
+    public static immutable bool debug_flag = false;
+
+}

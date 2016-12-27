a89fab0719c249bf7e87df114b397c1775a3fead
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2016-12-27 18:14:16 +0100

    more transition classes

--- src/antlr/v4/runtime/atn/PrecedencePredicateTransition.d
+++ src/antlr/v4/runtime/atn/PrecedencePredicateTransition.d
@@ -1,6 +1,11 @@
 module antlr.v4.runtime.atn.PrecedencePredicateTransition;
 
+import std.conv;
 import antlr.v4.runtime.atn.AbstractPredicateTransition;
+import antlr.v4.runtime.atn.ATNState;
+import antlr.v4.runtime.atn.Predicate;
+import antlr.v4.runtime.atn.TransitionStates;
+import antlr.v4.runtime.atn.PrecedencePredicate;
 
 // Class PrecedencePredicateTransition
 /**
@@ -14,5 +19,52 @@ class PrecedencePredicateTransition : AbstractPredicateTransition
      * @final
      */
     public int precedence;
+
+    public this(ATNState target, int precedence)
+    {
+        super(target);
+        this.precedence = precedence;
+    }
+
+    /**
+     * @uml
+     * @override
+     */
+    public override int getSerializationType()
+    {
+        return TransitionStates.PRECEDENCE;
+    }
+
+    /**
+     * @uml
+     * @override
+     */
+    public override bool isEpsilon()
+    {
+        return true;
+    }
+
+    /**
+     * @uml
+     * @override
+     */
+    public override bool matches(int symbol, int minVocabSymbol, int maxVocabSymbol)
+    {
+        return false;
+    }
+
+    public Predicate getPredicate()
+    {
+        return new PrecedencePredicate(precedence);
+    }
+
+    /**
+     * @uml
+     * @override
+     */
+    public override string toString()
+    {
+        return to!string(precedence) ~ " >= _p";
+    }
 
 }

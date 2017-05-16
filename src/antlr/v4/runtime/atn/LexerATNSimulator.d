13aec10f7507c919687c2a9ff890d941ce8ebee2
Egbert Voigt <Egbert.Voigt@gmx.de>
2017-05-16 08:29:10 +0200

    new class and functions

--- src/antlr/v4/runtime/atn/LexerATNSimulator.d
+++ src/antlr/v4/runtime/atn/LexerATNSimulator.d
@@ -32,9 +32,14 @@
 module antlr.v4.runtime.atn.LexerATNSimulator;
 
 import antlr.v4.runtime.atn.ATNSimulator;
+import antlr.v4.runtime.IntStream;
 import antlr.v4.runtime.Lexer;
 import antlr.v4.runtime.dfa.DFA;
+import antlr.v4.runtime.dfa.DFAState;
 import antlr.v4.runtime.atn.ATN;
+import antlr.v4.runtime.atn.ATNState;
+import antlr.v4.runtime.atn.ATNConfigSet;
+import antlr.v4.runtime.atn.LexerActionExecutor;
 import antlr.v4.runtime.atn.PredictionContextCache;
 import antlr.v4.runtime.atn.SimState;
 import antlr.v4.runtime.CharStream;
@@ -156,9 +161,158 @@ class LexerATNSimulator : ATNSimulator
 
     protected int matchATN(CharStream input)
     {
+	ATNState startState = atn.modeToStartState[mode];
+        debug
+            writefln("matchATN mode %1$d start: %2$s\n", mode, startState);
+        int old_mode = mode;
+
+        ATNConfigSet s0_closure = computeStartState(input, startState);
+        bool suppressEdge = s0_closure.hasSemanticContext;
+
+        s0_closure.hasSemanticContext = false;
+        DFAState next = addDFAState(s0_closure);
+        if (!suppressEdge) {
+            decisionToDFA[mode].s0 = next;
+        }
+
+        int predict = execATN(input, next);
+
+        debug
+            writefln("DFA after matchATN: %1$s\n", decisionToDFA[old_mode].toLexerString());
+
+        return predict;
     }
 
-    protected int execATN(CharStream input)
+    protected int execATN(CharStream input, DFAState ds0)
+    {
+	//System.out.println("enter exec index "+input.index()+" from "+ds0.configs);
+        debug {
+            writefln("start state closure=%s\n", ds0.configs);
+        }
+
+        if (ds0.isAcceptState) {
+            // allow zero-length tokens
+            captureSimState(prevAccept, input, ds0);
+        }
+
+        int t = input.LA(1);
+
+        DFAState s = ds0; // s is current/from DFA state
+
+        while (true) { // while more work
+            debug {
+                writefln("execATN loop starting closure: %s\n", s.configs);
+            }
+            // As we move src->trg, src->trg, we keep track of the previous trg to
+            // avoid looking up the DFA state again, which is expensive.
+            // If the previous target was already part of the DFA, we might
+            // be able to avoid doing a reach operation upon t. If s!=null,
+            // it means that semantic predicates didn't prevent us from
+            // creating a DFA state. Once we know s!=null, we check to see if
+            // the DFA state has an edge already for t. If so, we can just reuse
+            // it's configuration set; there's no point in re-computing it.
+            // This is kind of like doing DFA simulation within the ATN
+            // simulation because DFA simulation is really just a way to avoid
+            // computing reach/closure sets. Technically, once we know that
+            // we have a previously added DFA state, we could jump over to
+            // the DFA simulator. But, that would mean popping back and forth
+            // a lot and making things more complicated algorithmically.
+            // This optimization makes a lot of sense for loops within DFA.
+            // A character will take us back to an existing DFA state
+            // that already has lots of edges out of it. e.g., .* in comments.
+            DFAState target = getExistingTargetState(s, t);
+            if (target is null) {
+                target = computeTargetState(input, s, t);
+            }
+
+            if (target == ERROR) {
+                break;
+            }
+            // If this is a consumable input element, make sure to consume before
+            // capturing the accept state so the input index, line, and char
+            // position accurately reflect the state of the interpreter at the
+            // end of the token.
+            if (t != IntStream.EOF) {
+                consume(input);
+            }
+
+            if (target.isAcceptState) {
+                captureSimState(prevAccept, input, target);
+                if (t == IntStream.EOF) {
+                    break;
+                }
+            }
+
+            t = input.LA(1);
+            s = target; // flip; current DFA target becomes new src/from state
+        }
+
+        return failOrAccept(prevAccept, input, s.configs, t);
+    }
+
+    /**
+     * @uml
+     * Get an existing target state for an edge in the DFA. If the target state
+     * for the edge has not yet been computed or is otherwise not available,
+     * this method returns {@code null}.
+     *
+     *  @param s The current DFA state
+     *  @param t The next input symbol
+     *  @return The existing target DFA state for the given input symbol
+     * {@code t}, or {@code null} if the target state for this edge is not
+     * already cached
+     */
+    public DFAState getExistingTargetState(DFAState s, int t)
+    {
+	if (s.edges is null || t < MIN_DFA_EDGE || t > MAX_DFA_EDGE) {
+            return null;
+        }
+
+        DFAState target = s.edges[t - MIN_DFA_EDGE];
+        if (target !is null)
+            debug {
+                writefln("reuse state %1$s"~
+                         " edge to %2$s", s.stateNumber, target.stateNumber);
+            }
+
+        return target;
+    }
+
+    /**
+     * @uml
+     * Compute a target state for an edge in the DFA, and attempt to add the
+     * computed state and corresponding edge to the DFA.
+     *
+     *  @param input The input stream
+     *  @param s The current DFA state
+     *  @param t The next input symbol
+     *
+     *  @return The computed target DFA state for the given input symbol
+     * {@code t}. If {@code t} does not lead to a valid DFA state, this method
+     * returns {@link #ERROR}.
+     */
+    protected DFAState computeTargetState(CharStream input, DFAState s, int t)
+    {
+    }
+
+    protected int failOrAccept(SimState prevAccept, CharStream input, ATNConfigSet reach,
+        int t)
+    {
+    }
+
+    /**
+     * @uml
+     * Given a starting configuration set, figure out all ATN configurations
+     * we can reach upon input {@code t}. Parameter {@code reach} is a return
+     * parameter.
+     */
+    protected void getReachableConfigSet(CharStream input, ATNConfigSet closure, ATNConfigSet reach,
+        int t)
+    {
+    }
+
+    protected void accept(CharStream input, LexerActionExecutor lexerActionExecutor, int startIndex,
+        int index, int line, int charPos)
     {
     }
 
---
+++ src/antlr/v4/runtime/atn/LexerActionExecutor.d
@@ -0,0 +1,19 @@
+module antlr.v4.runtime.atn.LexerActionExecutor;
+
+// Class LexerActionExecutor
+/**
+ * @uml
+ * Represents an executor for a sequence of lexer actions which traversed during
+ * the matching operation of a lexer rule (token).
+ *
+ * <p>The executor tracks position information for position-dependent lexer actions
+ * efficiently, ensuring that actions appearing only at the end of the rule do
+ * not cause bloating of the {@link DFA} created for the lexer.</p>
+ *
+ *  @author Sam Harwell
+ *  @since 4.2
+ */
+class LexerActionExecutor
+{
+
+}

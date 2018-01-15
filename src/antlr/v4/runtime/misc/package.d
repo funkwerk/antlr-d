5f5e8f81ae6b16c2d66b80c6901f398cf160e6b0
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2018-01-15 14:13:10 +0100

    cleaning workspace

--- src/antlr/v4/runtime/misc/package.d
+++ src/antlr/v4/runtime/misc/package.d
@@ -3,3 +3,6 @@ module antlr.v4.runtime.misc;
 public import antlr.v4.runtime.misc.BitSet;
 public import antlr.v4.runtime.misc.DoubleKeyMap;
 public import antlr.v4.runtime.misc.IntervalSet;
+public import antlr.v4.runtime.misc.AbstractEqualityComparator;
+public import antlr.v4.runtime.misc.Array2DHashSet;
+public import antlr.v4.runtime.misc.Interval;

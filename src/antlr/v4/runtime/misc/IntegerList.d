ef4d19b05824924e200abc9a646c4a86d926000d
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2018-02-06 17:08:50 +0100

    more unittests

--- src/antlr/v4/runtime/misc/IntegerList.d
+++ src/antlr/v4/runtime/misc/IntegerList.d
@@ -1,32 +1,7 @@
 /*
- * [The "BSD license"]
- *  Copyright (c) 2012 Terence Parr
- *  Copyright (c) 2012 Sam Harwell
- *  Copyright (c) 2017 Egbert Voigt
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *  1. Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *  3. The name of the author may not be used to endorse or promote products
- *     derived from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2012-2018 The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
  */
 
 module antlr.v4.runtime.misc.IntegerList;
@@ -50,7 +25,7 @@ class IntegerList
 
     private static immutable int INITIAL_SIZE = 4;
 
-    private static immutable int MAX_ARRAY_SIZE  = int.max-8;
+    private static immutable int MAX_ARRAY_SIZE  = int.max - 8;
 
     /**
      * @uml
@@ -237,7 +212,6 @@ class IntegerList
     }
 
     /**
-     * @uml
      * Compares the specified object with this list for equality.  Returns
      * {@code true} if and only if the specified object is also an {@link IntegerList},
      * both lists have the same size, and all corresponding pairs of elements in
@@ -273,7 +247,6 @@ class IntegerList
     }
 
     /**
-     * @uml
      * Returns the hash code value for this list.
      *
      * <p>This implementation uses exactly the code that is used to define the
@@ -292,8 +265,8 @@ class IntegerList
     }
 
     /**
-     * @uml
      * Returns a string representation of this list.
+     * @uml
      * @override
      */
     public override string toString()
@@ -345,4 +318,26 @@ class IntegerList
         return this.size_;
     }
 
+}
+
+version(unittest) {
+    import fluent.asserts;
+    import unit_threaded;
+
+    class Test {
+
+        @Tags("integerList")
+        @("testEmpty")
+        unittest {
+            auto il = new IntegerList;
+            il.should.not.beNull;
+        }
+
+        @Tags("integerList")
+        @("testCapacity")
+        unittest {
+            auto il = new IntegerList(20);
+            il.should.not.beNull;
+        }
+    }
 }

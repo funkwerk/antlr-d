46d8d2d7755a5cfe483ff50ad34751f9b70b501b
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2018-02-16 17:02:04 +0100

    review EmptyPredictionContext

--- src/antlr/v4/runtime/atn/package.d
+++ src/antlr/v4/runtime/atn/package.d
@@ -3,3 +3,4 @@ module antlr.v4.runtime.atn;
 public import antlr.v4.runtime.atn.ATNState;
 public import antlr.v4.runtime.atn.PredictionContext;
 public import antlr.v4.runtime.atn.ContextID;
+public import antlr.v4.runtime.atn.SingletonPredictionContext;

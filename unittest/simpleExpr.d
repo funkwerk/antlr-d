92334e80e05486719c1e5f82959f0ce4f1d8e72a
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2018-06-17 15:32:29 +0200

    some minor changes, more use od dshould unittest

--- unittest/simpleExpr.d
+++ unittest/simpleExpr.d
@@ -6,7 +6,7 @@ import antlr.v4.runtime.LexerNoViableAltException;
 import ExprLexer;
 import ExprParser;
 import ExprBaseListener;
-import fluent.asserts : should, Assert;
+import dshould : be, equal, not, should;
 import unit_threaded;
 
 
@@ -31,38 +31,35 @@ class Test {
         
         auto input = "100\n122\n";
         auto antlrInput = new ANTLRInputStream(input);
-        antlrInput.should.not.beNull;
+        antlrInput.should.not.be(null);
         auto lexer = new ExprLexer(antlrInput);
-        lexer.should.not.beNull;
+        lexer.should.not.be(null);
         lexer.getGrammarFileName.should.equal("Expr.g4");
         lexer.getRuleNames.should.equal(["T__0", "T__1",
                                          "T__2", "T__3",
                                          "T__4", "T__5",
                                          "NEWLINE", "INT"]);
         auto cts = new CommonTokenStream(lexer);
-        cts.should.not.beNull;
+        cts.should.not.be(null);
         cts.getNumberOfOnChannelTokens.should.equal(5);
-        Assert.equal((cast(CommonToken)cts.LT(1)).toString,
-                     "[@0,0:2='100',<8>,1:0]");
-        Assert.equal((cast(CommonToken)cts.LT(2)).toString,
-                     "[@1,3:3='\\n',<7>,1:3]");
-        Assert.equal((cast(CommonToken)cts.LT(3)).toString,
-                     "[@2,4:6='122',<8>,2:0]");
-        Assert.equal((cast(CommonToken)cts.LT(4)).toString,
-                     "[@3,7:7='\\n',<7>,2:3]");
+        (cast(CommonToken)cts.LT(1)).toString.should.equal("[@0,0:2='100',<8>,1:0]");
+        (cast(CommonToken)cts.LT(2)).toString.should.equal("[@1,3:3='\\n',<7>,1:3]");
+        (cast(CommonToken)cts.LT(3)).toString.should.equal("[@2,4:6='122',<8>,2:0]");
+        (cast(CommonToken)cts.LT(4)).toString.should.equal("[@3,7:7='\\n',<7>,2:3]");
+
         // Pass the tokens to the parser
         ExprParser parser = new ExprParser(cts);
-        // Specify our entry point
+        // Entry point
         ExprParser.ExprParser.ProgContext progContext = parser.prog;
-        Assert.equal(progContext.children.length, 4);
-        Assert.equal((cast(CommonToken)progContext.start).toString, "[@0,0:2='100',<8>,1:0]");
-        Assert.equal((cast(CommonToken)progContext.stop).toString, "[@3,7:7='\\n',<7>,2:3]");
-        Assert.equal(progContext.children[0].classinfo == RuleNode.classinfo, true);
+        progContext.children.length.should.equal(4);
+        (cast(CommonToken)progContext.start).toString.should.equal("[@0,0:2='100',<8>,1:0]");
+        (cast(CommonToken)progContext.stop).toString.should.equal("[@3,7:7='\\n',<7>,2:3]");
+        progContext.children[0].classinfo.should.equal(RuleNode.classinfo);
         import antlr.v4.runtime.tree.TerminalNode;
-        Assert.equal(progContext.children[1].classinfo == TerminalNode.classinfo, true);
-        Assert.equal(progContext.children[2].classinfo == RuleNode.classinfo, true);
-        Assert.equal(progContext.children[3].classinfo == TerminalNode.classinfo, true);
-        Assert.equal((cast(RuleContext)progContext.getChild(0)).getText, "100");
+        progContext.children[1].classinfo.should.equal(TerminalNode.classinfo);
+        progContext.children[2].classinfo.should.equal(RuleNode.classinfo);
+        progContext.children[3].classinfo.should.equal(TerminalNode.classinfo);
+        (cast(RuleContext)progContext.getChild(0)).getText.should.equal("100");
         import antlr.v4.runtime.tree.ParseTreeWalker;
         auto baseLis = new ExprListener;
         auto walker = new ParseTreeWalker;
@@ -75,37 +72,34 @@ class Test {
     unittest {
         auto input = "(100)\n";
         auto antlrInput = new ANTLRInputStream(input);
-        antlrInput.should.not.beNull;
+        antlrInput.should.not.be(null);
         auto lexer = new ExprLexer(antlrInput);
-        lexer.should.not.beNull;
+        lexer.should.not.be(null);
         lexer.getGrammarFileName.should.equal("Expr.g4");
         lexer.getRuleNames.should.equal(["T__0", "T__1",
                                          "T__2", "T__3",
                                          "T__4", "T__5",
                                          "NEWLINE", "INT"]);
         auto cts = new CommonTokenStream(lexer);
-        cts.should.not.beNull;
+        cts.should.not.be(null);
         cts.getNumberOfOnChannelTokens.should.equal(5);
-        Assert.equal((cast(CommonToken)cts.LT(1)).toString,
-                     "[@0,0:0='(',<5>,1:0]");
-        Assert.equal((cast(CommonToken)cts.LT(2)).toString,
-                     "[@1,1:3='100',<8>,1:1]");
-        Assert.equal((cast(CommonToken)cts.LT(3)).toString,
-                     "[@2,4:4=')',<6>,1:4]");
+        (cast(CommonToken)cts.LT(1)).toString.should.equal("[@0,0:0='(',<5>,1:0]");
+        (cast(CommonToken)cts.LT(2)).toString.should.equal("[@1,1:3='100',<8>,1:1]");
+        (cast(CommonToken)cts.LT(3)).toString.should.equal("[@2,4:4=')',<6>,1:4]");
         // Pass the tokens to the parser
         ExprParser parser = new ExprParser(cts);
-        // Specify our entry point
+        // Entry point
         ExprParser.ExprParser.ProgContext progContext = parser.prog;
-        Assert.equal(progContext.children.length, 2);
-        Assert.equal((cast(CommonToken)progContext.start).toString, "[@0,0:0='(',<5>,1:0]");
-        Assert.equal((cast(CommonToken)progContext.stop).toString, "[@3,5:5='\\n',<7>,1:5]");
+        progContext.children.length.should.equal(2);
+        (cast(CommonToken)progContext.start).toString.should.equal("[@0,0:0='(',<5>,1:0]");
+        (cast(CommonToken)progContext.stop).toString.should.equal("[@3,5:5='\\n',<7>,1:5]");
 
         import antlr.v4.runtime.RuleContext;
         import antlr.v4.runtime.tree.RuleNode;
-        Assert.equal(progContext.children[0].classinfo == RuleNode.classinfo, true);
+        progContext.children[0].classinfo.should.equal(RuleNode.classinfo);
         import antlr.v4.runtime.tree.TerminalNode;
-        Assert.equal(progContext.children[1].classinfo == TerminalNode.classinfo, true);
-        Assert.equal((cast(RuleContext)progContext).toStringTree(parser),
+        progContext.children[1].classinfo.should.equal(TerminalNode.classinfo);
+        (cast(RuleContext)progContext).toStringTree(parser).should.equal(
                      "([] ([4] [@0,0:0='(',<5>,1:0] ([15 4] [@1,1:3='100',<8>,1:1]) " ~
                      "[@2,4:4=')',<6>,1:4]) [@3,5:5='\\n',<7>,1:5])[\"prog\", \"expr\"]");
     }
@@ -115,27 +109,20 @@ class Test {
     unittest {
         auto input = "100*13\n";
         auto antlrInput = new ANTLRInputStream(input);
-        antlrInput.should.not.beNull;
+        antlrInput.should.not.be(null);
         auto lexer = new ExprLexer(antlrInput);
-        lexer.should.not.beNull;
+        lexer.should.not.be(null);
         lexer.getGrammarFileName.should.equal("Expr.g4");
         lexer.getRuleNames.should.equal(["T__0", "T__1", "T__2", "T__3",
                                          "T__4", "T__5", "NEWLINE", "INT"]);
         auto cts = new CommonTokenStream(lexer);
-        cts.should.not.beNull;
         cts.getNumberOfOnChannelTokens.should.equal(5);
-        Assert.equal((cast(CommonToken)cts.LT(1)).toString,
-                     "[@0,0:2='100',<8>,1:0]");
-        Assert.equal((cast(CommonToken)cts.LT(2)).toString,
-                     "[@1,3:3='*',<1>,1:3]");
-        Assert.equal((cast(CommonToken)cts.LT(3)).toString,
-                     "[@2,4:5='13',<8>,1:4]");
         // Pass the tokens to the parser
         ExprParser parser = new ExprParser(cts);
         // Specify our entry point
         ExprParser.ExprParser.ProgContext progContext = parser.prog;
-        Assert.equal(progContext.children.length, 2);
-        Assert.equal((cast(RuleContext)progContext).toStringTree(parser),
+        progContext.children.length.should.equal(2);
+        (cast(RuleContext)progContext).toStringTree(parser).should.equal(
                      "([] ([4] ([2 4] [@0,0:2='100',<8>,1:0]) " ~
                      "[@1,3:3='*',<1>,1:3] ([22 4] [@2,4:5='13',<8>,1:4])) " ~
                      "[@3,6:6='\\n',<7>,1:6])[\"prog\", \"expr\"]");
@@ -146,28 +133,24 @@ class Test {
     unittest {
         auto input = "(100+2)*3\n";
         auto antlrInput = new ANTLRInputStream(input);
-        antlrInput.should.not.beNull;
+        antlrInput.should.not.be(null);
         auto lexer = new ExprLexer(antlrInput);
-        lexer.should.not.beNull;
+        lexer.should.not.be(null);
         lexer.getGrammarFileName.should.equal("Expr.g4");
         lexer.getRuleNames.should.equal(["T__0", "T__1",
                                          "T__2", "T__3",
                                          "T__4", "T__5",
                                          "NEWLINE", "INT"]);
         auto cts = new CommonTokenStream(lexer);
-        cts.should.not.beNull;
         cts.getNumberOfOnChannelTokens.should.equal(9);
-        Assert.equal((cast(CommonToken)cts.LT(1)).toString,
-                     "[@0,0:0='(',<5>,1:0]");
-        Assert.equal((cast(CommonToken)cts.LT(2)).toString,
-                     "[@1,1:3='100',<8>,1:1]");
-        Assert.equal((cast(CommonToken)cts.LT(3)).toString,
-                     "[@2,4:4='+',<3>,1:4]");
+        (cast(CommonToken)cts.LT(1)).toString.should.equal("[@0,0:0='(',<5>,1:0]");
+        (cast(CommonToken)cts.LT(2)).toString.should.equal("[@1,1:3='100',<8>,1:1]");
+        (cast(CommonToken)cts.LT(3)).toString.should.equal("[@2,4:4='+',<3>,1:4]");
         // Pass the tokens to the parser
         ExprParser parser = new ExprParser(cts);
         // Specify our entry point
         ExprParser.ExprParser.ProgContext progContext = parser.prog;
-        Assert.equal((cast(RuleContext)progContext).toStringTree(parser),
+        (cast(RuleContext)progContext).toStringTree(parser).should.equal(
                      "([] ([4] ([2 4] [@0,0:0='(',<5>,1:0] ([15 2 4] "~
                      "([2 15 2 4] [@1,1:3='100',<8>,1:1]) [@2,4:4='+',<3>,1:4] "~
                      "([25 15 2 4] [@3,5:5='2',<8>,1:5])) [@4,6:6=')',<6>,1:6]) "~

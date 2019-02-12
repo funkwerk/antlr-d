75ee79ecdcae1110be79dc21a9afbdf1cb9682ca
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2019-02-12 10:45:17 +0100

    more tests

--- source/antlr/v4/runtime/RewriteOperation.d
+++ source/antlr/v4/runtime/RewriteOperation.d
@@ -9,11 +9,12 @@ module antlr.v4.runtime.RewriteOperation;
 import antlr.v4.runtime.TokenStreamRewriter;
 import std.conv;
 import std.format;
+import std.variant;
 
 /**
  * TODO add class description
  */
-class RewriteOperation(T) : TokenStreamRewriter!T
+class RewriteOperation : TokenStreamRewriter
 {
 
     /**
@@ -26,24 +27,22 @@ class RewriteOperation(T) : TokenStreamRewriter!T
      */
     public size_t index;
 
-    public T text;
+    public Variant text;
 
     public this(size_t index)
     {
         this.index = index;
     }
 
-    public this(size_t index, T text)
+    public this(size_t index, Variant text)
     {
-        this.index = index;
-        this.text = text;
     }
 
     /**
      * Execute the rewrite operation by possibly adding to the buffer.
      *  Return the index of the next token to operate on.
      */
-    public size_t execute(ref T buf)
+    public size_t execute(ref Variant buf)
     {
         return index;
     }

8f1cc3e23872f9897fb89fbb9b522d16d9b1349c
Egbert Voigt <Egbert.Voigt@funkwerk-itk.com>
2019-02-18 15:31:21 +0100

    remove useless convert

--- source/antlr/v4/runtime/atn/PredictionContextCache.d
+++ source/antlr/v4/runtime/atn/PredictionContextCache.d
@@ -1,31 +1,7 @@
 /*
- * [The "BSD license"]
- *  Copyright (c) 2016 Terence Parr
- *  Copyright (c) 2016 Sam Harwell
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *  1. Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *  2. Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *  3. The name of the author may not be used to endorse or promote products
- *     derived from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
- *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2012-2019 The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
  */
 
 module antlr.v4.runtime.atn.PredictionContextCache;
@@ -34,7 +10,6 @@ import std.conv;
 import antlr.v4.runtime.atn.PredictionContext;
 
 /**
- * @uml
  * Used to cache {@link PredictionContext} objects. Its used for the shared
  * context cash associated with contexts in DFA states. This cache
  * can be used for both lexers and parsers.
@@ -45,7 +20,6 @@ class PredictionContextCache
     protected PredictionContext[PredictionContext] cache;
 
     /**
-     * @uml
      * Add a context to the cache and return it. If the context already exists,
      * return that one instead and do not add a new context to the cache.
      * Protect shared cache from unsafe thread access.
@@ -67,9 +41,9 @@ class PredictionContextCache
 	return cache[ctx];
     }
 
-    public int size()
+    public size_t size()
     {
-        return to!int(cache.length);
+        return cache.length;
     }
 
     public bool hasKey(PredictionContext predictionContext)
